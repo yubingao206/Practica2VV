a40bc48abf4a5f0aac00a7f8c68054599dcc5238
yubin gao <yubin.gao@alumnos.upm.es>
2025-11-28 11:38:38 +0100

    secound commit

---
+++ src/test/java/AlienTest.java
@@ -0,0 +1,149 @@
+import space_invaders.sprites.Alien;
+import org.junit.jupiter.api.Test;
+import space_invaders.sprites.Sprite;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AlienTest {
+
+    @Test
+    void testinitAlien_Caso1() {
+        int x = 100;
+        int y = 100;
+        Alien a = new Alien(x, y);
+
+        try {
+            Method metodo = a.getClass().getDeclaredMethod("initAlien", int.class, int.class);
+            metodo.setAccessible(true);
+            metodo.invoke(a, x,y);
+
+            Field valx = Sprite.class.getDeclaredField("x");
+            valx.setAccessible(true);
+            int valorx = (int) valx.get(a);
+
+            Field valy = Sprite.class.getDeclaredField("y");
+            valy.setAccessible(true);
+            int valory = (int) valy.get(a);
+
+            assertTrue((valorx == x) && (valory==y));
+
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testinitAlien_Caso3() {
+        int x = 100;
+        int y = 351;
+        Alien a = new Alien(x, y);
+
+        try {
+            Method metodo = a.getClass().getDeclaredMethod("initAlien", int.class, int.class);
+            metodo.setAccessible(true);
+            metodo.invoke(a, x,y);
+
+            Field valx = Sprite.class.getDeclaredField("x");
+            valx.setAccessible(true);
+            int valorx = (int) valx.get(a);
+
+            Field valy = Sprite.class.getDeclaredField("y");
+            valy.setAccessible(true);
+            int valory = (int) valy.get(a);
+
+            assertTrue((valorx == 100) && (valory == 351));
+
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testinitAlien_Caso4() {
+        int x = -1;
+        int y = 100;
+        Alien a = new Alien(x, y);
+
+        try {
+            Method metodo = a.getClass().getDeclaredMethod("initAlien", int.class, int.class);
+            metodo.setAccessible(true);
+            metodo.invoke(a, x,y);
+
+            Field valx = Sprite.class.getDeclaredField("x");
+            valx.setAccessible(true);
+            int valorx = (int) valx.get(a);
+
+            Field valy = Sprite.class.getDeclaredField("y");
+            valy.setAccessible(true);
+            int valory = (int) valy.get(a);
+
+            assertTrue((valorx == -1) && (valory == 100));
+
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testinitAlien_Caso5() {
+        int x = 359;
+        int y = 100;
+        Alien a = new Alien(x, y);
+
+        try {
+            Method metodo = a.getClass().getDeclaredMethod("initAlien", int.class, int.class);
+            metodo.setAccessible(true);
+            metodo.invoke(a, x,y);
+
+            Field valx = Sprite.class.getDeclaredField("x");
+            valx.setAccessible(true);
+            int valorx = (int) valx.get(a);
+
+            Field valy = Sprite.class.getDeclaredField("y");
+            valy.setAccessible(true);
+            int valory = (int) valy.get(a);
+
+            assertTrue((valorx == 359) && (valory == 100));
+
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testAct_Caso1(){
+        int direccion = -1;
+        int x = 100;
+        Alien a = new Alien(x,10);
+        a.act(direccion);
+        assertEquals(101,a.getX());
+    }
+}
---
+++ src/test/java/BombTest.java
@@ -0,0 +1,113 @@
+import main.Commons;
+import org.junit.jupiter.api.Test;
+import space_invaders.sprites.Alien;
+import space_invaders.sprites.Alien.Bomb;
+import space_invaders.sprites.Sprite;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class BombTest {
+
+    @Test
+    void testinitBomb_Caso1() {
+        int x = 359;
+        int y = 351;
+        Alien a = new Alien(x, y);
+        Alien.Bomb b = a.getBomb();
+
+        try {
+            Method metodo = b.getClass().getDeclaredMethod("initBomb", int.class, int.class);
+            metodo.setAccessible(true);
+            metodo.invoke(b, x,y);
+
+            Field valx = Sprite.class.getDeclaredField("x");
+            valx.setAccessible(true);
+            int valorx = (int) valx.get(b);
+
+            Field valy = Sprite.class.getDeclaredField("y");
+            valy.setAccessible(true);
+            int valory = (int) valy.get(b);
+
+            assertTrue((valorx == Commons.BOARD_WIDTH) && (valory==Commons.BOARD_HEIGHT));
+
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testinitBomb_Caso2() {
+        int x = 100;
+        int y = 351;
+        Alien a = new Alien(x, y);
+        Alien.Bomb b = a.getBomb();
+
+        try {
+            Method metodo = b.getClass().getDeclaredMethod("initBomb", int.class, int.class);
+            metodo.setAccessible(true);
+            metodo.invoke(b, x,y);
+
+            Field valx = Sprite.class.getDeclaredField("x");
+            valx.setAccessible(true);
+            int valorx = (int) valx.get(b);
+
+            Field valy = Sprite.class.getDeclaredField("y");
+            valy.setAccessible(true);
+            int valory = (int) valy.get(b);
+
+            assertTrue((valorx == Commons.BOARD_WIDTH) && (valory == Commons.BOARD_HEIGHT));
+
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Test
+    void testinitBomb_Caso3() {
+        int x = 100;
+        int y = 100;
+        Alien a = new Alien(x, y);
+        Alien.Bomb b = a.getBomb();
+
+        try {
+            Method metodo = b.getClass().getDeclaredMethod("initBomb", int.class, int.class);
+            metodo.setAccessible(true);
+            metodo.invoke(b, x,y);
+
+            Field valx = Sprite.class.getDeclaredField("x");
+            valx.setAccessible(true);
+            int valorx = (int) valx.get(b);
+
+            Field valy = Sprite.class.getDeclaredField("y");
+            valy.setAccessible(true);
+            int valory = (int) valy.get(b);
+
+            assertTrue((valorx == 200) && (valory == 200));
+
+        } catch (NoSuchMethodException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchFieldException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
